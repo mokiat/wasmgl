e1b20e3ae7bd7929554003e86fcfcdcc6c21ed68
Momchil Atanasov <momchil.dev@gmail.com>
2022-03-20 12:28:43 +0200

    Improve types

--- types.go
+++ types.go
@@ -4,19 +4,45 @@ package wasmgl
 
 import "syscall/js"
 
+var NilAttribLocation = AttribLocation(js.Null())
+
 type AttribLocation js.Value
+
+func (l AttribLocation) IsValid() bool {
+	return isSpecified(js.Value(l))
+}
+
+var NilBuffer = Buffer(js.Null())
 
 type Buffer js.Value
 
-var NullBuffer = Buffer(js.Null())
+func (b Buffer) IsValid() bool {
+	return isSpecified(js.Value(b))
+}
+
+var NilFramebuffer = Framebuffer(js.Null())
 
 type Framebuffer js.Value
 
-var NullFramebuffer = Framebuffer(js.Null())
+func (f Framebuffer) IsValid() bool {
+	return isSpecified(js.Value(f))
+}
+
+var NilProgram = Program(js.Null())
 
 type Program js.Value
 
+func (p Program) IsValid() bool {
+	return isSpecified(js.Value(p))
+}
+
+var NilResult = Result(js.Null())
+
 type Result js.Value
+
+func (r Result) IsValid() bool {
+	return isSpecified(js.Value(r))
+}
 
 func (r Result) Bool() bool {
 	return js.Value(r).Bool()
@@ -26,26 +52,46 @@ func (r Result) Int() int {
 	return js.Value(r).Int()
 }
 
+var NilShader = Shader(js.Null())
+
 type Shader js.Value
+
+func (s Shader) IsValid() bool {
+	return isSpecified(js.Value(s))
+}
+
+var NilTexture = Texture(js.Null())
 
 type Texture js.Value
 
-var NullTexture = Texture(js.Null())
+func (t Texture) IsValid() bool {
+	return isSpecified(js.Value(t))
+}
+
+var NilUniformLocation = UniformLocation(js.Null())
 
 type UniformLocation js.Value
 
-func (l UniformLocation) Valid() bool {
-	return !js.Value(l).IsUndefined() && !js.Value(l).IsNull()
+func (l UniformLocation) IsValid() bool {
+	return isSpecified(js.Value(l))
 }
+
+var NilVertexArray = VertexArray(js.Null())
 
 type VertexArray js.Value
 
-var NullVertexArray = VertexArray(js.Null())
+func (a VertexArray) IsValid() bool {
+	return isSpecified(js.Value(a))
+}
+
+var NilSync = Sync(js.Null())
 
 type Sync js.Value
 
 func (s Sync) Valid() bool {
-	return !js.Value(s).IsUndefined() && !js.Value(s).IsNull()
+	return isSpecified(js.Value(s))
 }
 
-var NullSync = Sync(js.Null())
+func isSpecified(jsValue js.Value) bool {
+	return !jsValue.IsUndefined() && !jsValue.IsNull()
+}
